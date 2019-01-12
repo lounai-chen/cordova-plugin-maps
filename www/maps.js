b39425b3b655205b4e3dafd24bb941d0806b8f6d
May.lou <2419538202@qq.com>
2019-01-12 20:50:47 +0800

    Update maps.js

--- www/maps.js
+++ www/maps.js
@@ -1,13 +1,17 @@
 var exec = require('cordova/exec');
 
 var maps = {
-    //我的位置
+    //打开第三方地图：我的位置
     open:function(success,error){
         exec(success, error, 'Maps', 'open', []);
     },
     //GPS导航  arg0：经度  arg1：纬度
     gps:function(success,error,arg0,arg1){
         exec(success, error, 'Maps', 'gps', [arg0,arg1]);
+    }
+    // 获取我当前的位置 IOS，返回GPS
+    getMyLocation:function(success,error){
+        exec(success, error, 'Maps', 'gps', []);
     }
 }
 
