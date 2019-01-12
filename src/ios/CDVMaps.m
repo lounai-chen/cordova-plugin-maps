a114636c71d87deae490bcbfffb1cb90aa57c2a6
May.lou <2419538202@qq.com>
2019-01-12 18:03:44 +0800

    Update CDVMaps.m

--- src/ios/CDVMaps.m
+++ src/ios/CDVMaps.m
@@ -24,28 +24,28 @@
 - (void)gps:(CDVInvokedUrlCommand*)command
 {
     // 获取传来的参数
-    NSString *longitude = [command.arguments objectAtIndex:0];  // 经度
-    NSString *latitude = [command.arguments objectAtIndex:1];   // 纬度
+    NSString *urlsting = [command.arguments objectAtIndex:0];
     NSLog(@"开始导航。。");
-    NSLog(longitude); 
-    NSLog(latitude); 
-
+    NSLog(urlsting);
+ 
    @try{
         if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"iosamap://"]]) {
             // 高德地图
-            NSString *urlsting =[[NSString stringWithFormat:@"iosamap://navi?sourceApplication=applicationName&poiname=fangheng&poiid=BGVIS&lat=%@&lon=%@&dev=1&style=2",latitude,longitude]stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+            //NSString *tep = @"iosamap://navi?sourceApplication=applicationName&poiname=fangheng&poiid=BGVIS&lat=22.537422&lon=113.975095&dev=0&style=2";
+            //NSString *urlsting =[[NSString stringWithFormat:@"iosamap://navi?sourceApplication=applicationName&poiname=fangheng&poiid=BGVIS&lat=%.6f&lon=%.6f&dev=0&style=2",d_latitude,d_longitude]stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+            //NSLog(urlsting);
             [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlsting] options:nil completionHandler:^(BOOL success) {
                     NSLog(@"scheme调用高德地图结束");    
             }];
         } 
-        else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"baidumap://"]]) {
-            // 百度地图
-            NSString *urlString = [[NSString stringWithFormat:@"baidumap://map/direction?region=beijing&origin=%@,%@&destination=西直门&coord_type=bd09ll&mode=driving&src=andr.baidu.openAPIdemo", latitude, longitude] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];       		
-            NSURL *url = [NSURL URLWithString:urlString];
-            [[UIApplication sharedApplication] openURL:url options:nil completionHandler:^(BOOL success) {
-                NSLog(@"scheme调用百度地图结束");  
-            }];
-        }
+//        else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"baidumap://"]]) {
+//            // 百度地图
+//            NSString *urlString = [[NSString stringWithFormat:@"baidumap://map/direction?region=beijing&origin=%@,%@&destination=西直门&coord_type=bd09ll&mode=driving&src=andr.baidu.openAPIdemo", latitude, longitude] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+//            NSURL *url = [NSURL URLWithString:urlString];
+//            [[UIApplication sharedApplication] openURL:url options:nil completionHandler:^(BOOL success) {
+//                NSLog(@"scheme调用百度地图结束");
+//            }];
+    //    }
         else{
             //没有安装地图APP
             
@@ -61,7 +61,7 @@
     }
     @finally {
         //最新版IOS12.1的canOpenURL判断有误， 再试一次高德地图，却可以打开
-        NSString *urlsting =[[NSString stringWithFormat:@"iosamap://navi?sourceApplication=applicationName&poiname=fangheng&poiid=BGVIS&lat=%@&lon=%@&dev=1&style=2",latitude,longitude]stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+//        NSString *urlsting =[[NSString stringWithFormat:@"iosamap://navi?sourceApplication=applicationName&poiname=fangheng&poiid=BGVIS&lat=%@&lon=%@&dev=1&style=2",latitude,longitude]stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlsting] options:nil completionHandler:^(BOOL success) {
              NSLog(@"scheme调用高德地图结束");    
         }];
